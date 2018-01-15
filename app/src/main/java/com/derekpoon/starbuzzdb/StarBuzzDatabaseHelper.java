4f9bdeadff0f1b7e45ca987c652a3396246ecb54
derekpoon <thebevil@gmail.com>
2018-01-15 20:29:44 +0000

    updated comments

--- app/src/main/java/com/derekpoon/starbuzzdb/StarBuzzDatabaseHelper.java
+++ app/src/main/java/com/derekpoon/starbuzzdb/StarBuzzDatabaseHelper.java
@@ -27,27 +27,27 @@ class StarBuzzDatabaseHelper extends SQLiteOpenHelper {
     //using it to create the table and insert data
     @Override
     public void onCreate(SQLiteDatabase db) {
-
         updateMyDatabase(db, 0, DB_VERSION);
-
     }
 
     //onUpgrade gets called when the database needs to be upgraded
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
         updateMyDatabase(db, oldVersion, newVersion);
-
     }
 
     private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (oldVersion < 1) {
+            // this is the SQL command to create the table
+            // _id column is the primary key
+            // table name = DRINK
             db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, " //create the drink table
+                    //below are the table columns
                     + "NAME TEXT, "
                     + "DESCRIPTION TEXT, "
                     + "IMAGE_RESOURCE_ID INTEGER);");
 
-            //insert each drink in a separate row
+            //insert each drink in a separate row in the DRINK table
             insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
             insertDrink(db, "Cappuccino", "Espresso, hot milk and steamed-milk foam",
                     R.drawable.cappuccino);
@@ -61,10 +61,20 @@ class StarBuzzDatabaseHelper extends SQLiteOpenHelper {
 
     //this method inserts several drinks
     private static void insertDrink(SQLiteDatabase db, String name, String description, int resourceId) {
+
+        //ContentValues opbject is used to hold name / value pairs of data
         ContentValues drinkValues = new ContentValues();
+
+        //example
+//        drinkValues.put("NAME", "Latte");
+//        drinkValues.put("DESCRIPTION", "Espresso and steamed milk");
+//        drinkValues.put("IMAGE_RESOURCE_ID", R.drawable.latte);
+
         drinkValues.put("NAME", name);
         drinkValues.put("DESCRIPTION", description);
         drinkValues.put("IMAGE_RESOURCE_ID", resourceId);
+
+        //insert the values into the DRINK table
         db.insert("DRINK", null, drinkValues);
     }
 
